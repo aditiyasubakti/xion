7c064ca26cfcb2f83a552ca96611317b48aa8da3
aditiyasubakti <aditiyatampan@gmail.com>
2025-11-16 22:24:49 +0700

    update

--- database/migration/order.php
+++ database/migration/order.php
@@ -7,7 +7,7 @@ return function (Schema $schema) {
     $schema->create("order", function ($table) {
         $table->id();
         $table->integer("customer_id");
-        $table->foreign("customer_id", "id", "customer","id","users"); // <── RELASI OTOMATIS
+        $table->foreign("customer_id", "id", "customer"); // <── RELASI OTOMATIS
         $table->string("produk");
         $table->integer("harga");
         $table->timestamps();
